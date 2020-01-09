6262676b359e2c227cf0f37765f924281b1db517
Nikita Prajapati <nikiprajapati1999@gmail.com>
2020-01-09 10:34:44 +0530

    made pattern for Letter A

---
+++ A.py
@@ -0,0 +1,13 @@
+n=25
+for i in range(n):
+	for j in range(2*n):
+		if i==int(n/2):
+			if(j>=n-i and j<=(n+i)):
+				print('*',end=' ')
+			else:
+				print(' ',end=' ')
+		elif(j==n-i or j==(n+i)):
+			print('*',end=' ')
+		else:
+			print(' ',end=' ')
+	print()
